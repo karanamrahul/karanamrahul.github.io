bb8a9d2f1b7cde45c154534c6925f4be7b05c6dc
karanamrahul <rahulkarnam777@gmail.com>
2022-01-25 13:23:31 -0500

    Updated

---
+++ generate_footer.js
@@ -0,0 +1,5 @@
+document.writeln('\
+<div id="footer">\
+    <p>2021, Rahul Karanam</p>\
+</div>\
+');
---
+++ generate_header.js
@@ -0,0 +1,14 @@
+document.writeln('\
+<div id="header">\
+    <h1><a href="index.html">Rahul Karanam</a></h1>\
+</div>\
+<nav id="menu">\
+    <ul>\
+        <li><a id="index" href="index.html">Home</a></li>\
+        <li><a id="education" href="education.html">Education</a></li>\
+        <li><a id="projects" href="projects.html">Projects</a></li>\
+        <li><a id="publications" href="publications.html">Experience</a></li>\
+        <li><a id="teaching" href="teaching.html">Teaching</a></li>\
+    </ul>\
+</nav>\
+');
